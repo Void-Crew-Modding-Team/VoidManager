6271ba184b57f35af69d06b7330eda236adcb739
DragonFire47 <46509577+DragonFire47@users.noreply.github.com>
2024-03-05 16:27:07 -0800

    Minor Cleanup

--- VoidManager/MPModChecks/MultiplayerType.cs
+++ VoidManager/MPModChecks/MultiplayerType.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace VoidManager.MPModChecks
+﻿namespace VoidManager.MPModChecks
 {
     //MultiplayerType
     public enum MultiplayerType
