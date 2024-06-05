17d320d7b052a764a9046cd0b49b10c21f7c427e
DragonFire47 <46509577+DragonFire47@users.noreply.github.com>
2024-06-04 22:39:12 -0700

    Supressed compiler warning

--- VoidManager/MPModChecks/KickMessagePatches.cs
+++ VoidManager/MPModChecks/KickMessagePatches.cs
@@ -13,6 +13,7 @@ namespace VoidManager.MPModChecks
         class KickedPatch
         {
             [HarmonyPostfix]
+            [System.Diagnostics.CodeAnalysis.SuppressMessage("Method Declaration", "Harmony003:Harmony non-ref patch parameters modified", Justification = "N/A")]
             static void patch(Scene scene)
             {
                 if (scene.buildIndex == CloneStarConstants.MainMenuSceneIndex && KickTitle != null && KickMessage != null)
