de03221bfbb066bf7c2df1b4f59a8466e0237dd7
DragonFire47 <46509577+DragonFire47@users.noreply.github.com>
2024-09-03 17:38:43 -0700

    Disabled QuickJoin.

---
+++ VoidManager/MPModChecks/QuickJoinPatch.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+using UI.Matchmaking;
+
+namespace VoidManager.MPModChecks
+{
+    [HarmonyPatch(typeof(MatchMakingJoinPanel), "QuickJoinRequested")]
+    internal class QuickJoinPatch
+    {
+        static bool Postfix()
+        {
+            MenuScreenController.Instance.ShowMessagePopup("matchmaking_unable_join".GetLocalized("Terminals"), $"Quick Join disabled by {MyPluginInfo.USERS_PLUGIN_NAME}");
+            return false;
+        }
+    }
+}
