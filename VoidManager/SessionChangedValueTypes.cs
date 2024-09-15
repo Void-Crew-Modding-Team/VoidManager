1c169abaf4baa01400e9c14c85a58cb979deedeb
DragonFire47 <46509577+DragonFire47@users.noreply.github.com>
2024-09-15 10:54:35 -0700

    Added differentation between HostStartSession and HostCreateRoom to SCVT.cs

--- VoidManager/SessionChangedValueTypes.cs
+++ VoidManager/SessionChangedValueTypes.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
+﻿#pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
 namespace VoidManager
 {
     /// <summary>
@@ -16,15 +10,32 @@ namespace VoidManager
         {
             IsHost = isHost;
             CallType = callType;
-            StartedAsHost = startedAsHost;
+            CreatedRoomAsHost = startedAsHost;
             IsMod_Session = isMod_Session;
             HostHasMod = hostHasMod;
         }
 
         public bool IsHost;
+
+        /// <summary>
+        /// Reason for call of OnSessionChanged
+        /// </summary>
         public CallType CallType;
-        public bool StartedAsHost;
+
+        /// <summary>
+        /// Current PhotonRoom was created as host.
+        /// </summary>
+        public bool CreatedRoomAsHost;
+
+        /// <summary>
+        /// Current Session/Run was started as host.
+        /// </summary>
+        public bool StartedSessionAsHost;
         public bool IsMod_Session;
+
+        /// <summary>
+        /// Host has mod, based on GUID without version/hash checking.
+        /// </summary>
         public bool HostHasMod;
     }
 
@@ -33,9 +44,29 @@ namespace VoidManager
     /// </summary>
     public enum CallType
     {
-        Hosting,
+        /// <summary>
+        /// On PhotonRoom Creation. Might not run if the host drops.
+        /// </summary>
+        HostCreateRoom,
+
+        /// <summary>
+        /// On Load into hub or ship as MasterClient.
+        /// </summary>
+        HostStartSession,
+
+        /// <summary>
+        /// On Load into hub or ship as client.
+        /// </summary>
         Joining,
+
+        /// <summary>
+        /// On Host Change.
+        /// </summary>
         HostChange,
+
+        /// <summary>
+        /// On Session escalation from Mod_Local to Mod_Session.
+        /// </summary>
         SessionEscalated
     }
 
